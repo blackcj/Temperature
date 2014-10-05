6abf9287b157b26992b82f58308f03a6ea8ae02c
Chris Black <blackcj2@gmail.com>
2014-10-05 16:34:02 -0500

    Added an overlapping scroll effect to the current temperature page.

--- app/src/main/java/com/blackcj/temperature/activity/MainActivity.java
+++ app/src/main/java/com/blackcj/temperature/activity/MainActivity.java
@@ -125,7 +125,7 @@ public class MainActivity extends ActionBarActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
-            return true;
+            //return true;
         }
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/blackcj/temperature/fragment/CurrentTempFragment.java
+++ app/src/main/java/com/blackcj/temperature/fragment/CurrentTempFragment.java
@@ -1,16 +1,26 @@
 package com.blackcj.temperature.fragment;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.blackcj.core.view.ObservableScrollView;
+import com.blackcj.core.view.ScrollViewListener;
 import com.blackcj.temperature.R;
 import com.blackcj.temperature.activity.MainActivity;
 import com.blackcj.temperature.model.Temperature;
@@ -23,7 +33,7 @@ import butterknife.InjectView;
  * Created by Chris on 10/2/2014.
  */
 public class CurrentTempFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener,
-        TemperatureDataSource.TemperatureListener{
+        TemperatureDataSource.TemperatureListener, ScrollViewListener {
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -37,10 +47,25 @@ public class CurrentTempFragment extends Fragment implements SwipeRefreshLayout.
     @InjectView(R.id.current_humidity_text)
     TextView currentHumidityText;
 
-    @InjectView(R.id.swipeRefreshLayout_listView)
-    SwipeRefreshLayout mSwipeRefreshLayout;
+    ListFragmentSwipeRefreshLayout mSwipeRefreshLayout;
+
+    @InjectView(R.id.observable_scroll_view)
+    ObservableScrollView mObservableScrollView;
+
+    @InjectView(R.id.temp_layout)
+    RelativeLayout mTempLayout;
+
+    @InjectView(R.id.humidity_layout)
+    RelativeLayout mHumidityLayout;
+
+    @InjectView(R.id.light_layout)
+    RelativeLayout mLightLayout;
+
+    @InjectView(R.id.sun_icon)
+    RelativeLayout mSunIcon;
 
     protected int section_number;
+    protected DisplayMetrics metrics;
 
     public static CurrentTempFragment newInstance(int sectionNumber) {
         CurrentTempFragment f = new CurrentTempFragment();
@@ -55,6 +80,15 @@ public class CurrentTempFragment extends Fragment implements SwipeRefreshLayout.
         super.onAttach(activity);
         ((MainActivity) activity).onSectionAttached(
                 getArguments().getInt(ARG_SECTION_NUMBER));
+        metrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+    }
+
+    @Override
+    public void onActivityCreated (Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // Indicate that this fragment would like to influence the set of actions in the action bar.
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -70,18 +104,39 @@ public class CurrentTempFragment extends Fragment implements SwipeRefreshLayout.
 
         ButterKnife.inject(this, view);
 
+        setHasOptionsMenu(true);
+
+
         mDataSource = new TemperatureDataSource(this);
         mDataSource.getTemp();
 
-        mSwipeRefreshLayout.setOnRefreshListener(this);
+        // Temporarily removed to test functionality without the swipe to refresh
 
+        // Now create a SwipeRefreshLayout to wrap the fragment's content view
+        mSwipeRefreshLayout = new ListFragmentSwipeRefreshLayout(container.getContext());
+
+        // Add the list fragment's content view to the SwipeRefreshLayout, making sure that it fills
+        // the SwipeRefreshLayout
+        mSwipeRefreshLayout.addView(view,
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        // Make sure that the SwipeRefreshLayout will fill the fragment
+        mSwipeRefreshLayout.setLayoutParams(
+                new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+
+        mSwipeRefreshLayout.setOnRefreshListener(this);
         mSwipeRefreshLayout.setColorScheme(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
+        android.R.color.holo_green_light,
+        android.R.color.holo_orange_light,
+        android.R.color.holo_red_light);
+
+        mObservableScrollView.setScrollViewListener(this);
+        mObservableScrollView.setOverScrollMode(View.OVER_SCROLL_NEVER);
 
         // Now return the SwipeRefreshLayout as this fragment's content view
-        return view;
+        return mSwipeRefreshLayout;
     }
 
     @Override
@@ -98,6 +153,20 @@ public class CurrentTempFragment extends Fragment implements SwipeRefreshLayout.
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            mSwipeRefreshLayout.setRefreshing(true);
+            onRefresh();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onTemperature(Temperature temperature) {
         currentTempText.setText(temperature.toString());
         currentHumidityText.setText(temperature.humidity + "%");
@@ -106,5 +175,72 @@ public class CurrentTempFragment extends Fragment implements SwipeRefreshLayout.
     @Override
     public void onError() {
         Toast.makeText(this.getActivity(), "Error occurred.", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy) {
+        int yScroll = y;
+        if(y < 0) {
+            yScroll = 0;
+        }
+
+        double percentScrolled = (double)yScroll / (double)scrollView.getMaxScrollAmount();
+        mSunIcon.setPadding(0,(int)(getDPI(400, metrics) * (1 - percentScrolled)),0,0);
+        //mSunIcon.setPadding(0,getDPI(400, metrics),0,0);
+        int value = (int)(170.0 * (1.0 - percentScrolled));
+        value = Math.max(value, 70);
+        mTempLayout.setPadding(0,yScroll/2 - 10,0,0);
+        RelativeLayout.LayoutParams head_params = (RelativeLayout.LayoutParams)mTempLayout.getLayoutParams();
+        head_params.setMargins(0, yScroll / 2, 0, 0); //substitute parameters for left, top, right, bottom
+        mTempLayout.setLayoutParams(head_params);
+        mHumidityLayout.setPadding(0, yScroll / 2 + 10, 0, 0);
+
+        head_params = (RelativeLayout.LayoutParams)mLightLayout.getLayoutParams();
+        head_params.height = getDPI(value, metrics);
+        //Log.d("CurrentTempFragment", "SCROLL: " + value);
+        mLightLayout.setLayoutParams(head_params);
+    }
+
+    public static int getDPI(int size, DisplayMetrics metrics){
+        return (size * metrics.densityDpi) / DisplayMetrics.DENSITY_DEFAULT;
+    }
+
+    public class ListFragmentSwipeRefreshLayout extends SwipeRefreshLayout {
+
+        public ListFragmentSwipeRefreshLayout(Context context) {
+            super(context);
+        }
+
+        /**
+         * As mentioned above, we need to override this method to properly signal when a
+         * 'swipe-to-refresh' is possible.
+         *
+         * @return true if the {@link android.widget.ListView} is visible and can scroll up.
+         */
+        @Override
+        public boolean canChildScrollUp() {
+            final ObservableScrollView scrollView = mObservableScrollView;
+            if (scrollView.getVisibility() == View.VISIBLE) {
+                return canListViewScrollUp(scrollView);
+            } else {
+                return false;
+            }
+        }
+
+    }
+
+    /**
+     * Utility method to check whether a {@link ObservableScrollView} can scroll up from it's current position.
+     * Handles platform version differences, providing backwards compatible functionality where
+     * needed.
+     */
+    private static boolean canListViewScrollUp(ObservableScrollView listView) {
+        if (android.os.Build.VERSION.SDK_INT >= 14) {
+            // For ICS and above we can call canScrollVertically() to determine this
+            return true;//ViewCompat.canScrollVertically(listView, -1);
+        } else {
+            // Min SDK 14
+            return true;
+        }
     }
 }
